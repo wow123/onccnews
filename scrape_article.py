016ef4a4b9f28ed740f8584eaa0b5f6d04434a4d
wow123 <wowbies@gmail.com>
2016-11-22 11:34:02 +0800

    First commit of all files

---
+++ scrape_article.py
@@ -0,0 +1,98 @@
+import sys
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+from PyQt4.QtWebKit import *
+from lxml import html
+from bs4 import BeautifulSoup
+from jinja2 import Environment, FileSystemLoader
+import time, os
+
+#Take this class for granted.Just use result of rendering.
+class Render(QWebPage):
+  def __init__(self, url):
+    self.app = QApplication(sys.argv)
+    QWebPage.__init__(self)
+    self.loadFinished.connect(self._loadFinished)
+    self.mainFrame().load(QUrl(url))
+    self.app.exec_()
+
+  def _loadFinished(self, result):
+    self.frame = self.mainFrame()
+    self.app.quit()
+
+# Main program begins
+if len(sys.argv)<2:
+    print('Pls. pass the article url as parameter.')
+    print('Usage: python scrape_article.py <url>')
+    quit()  # Exit program
+
+# Define project home path
+project_path = '.'  # for Windows or RPi interactive
+#project_path = '/home/pi/projects/onccnews'  # for RPi cron job
+
+domain = 'http://orientaldaily.on.cc'
+oncc_url = str(domain + sys.argv[1])
+print('Scraping ' + oncc_url)
+#oncc_url = 'http://orientaldaily.on.cc/cnt/news/20160920/00186_001.html'
+i = articleId = oncc_url.rfind('/')  # Search last '/' char to get the aricle Id
+articleId = oncc_url[i+1:]
+
+# declare variable for jinja
+output_list=[]
+# Scape web page by lxml
+r = Render(oncc_url)
+# Parsing data by Beautiful Soup
+soup = BeautifulSoup(r.frame.toHtml(), 'html.parser')
+#print (soup.encode('utf-8'))
+title = soup.find('h1')  # Get aritcle title
+#print(title)
+heading = title.text
+output_list.append('<i><h1>' + heading + '</h1></i>')
+heading=title.text
+
+# Get text
+for paragh in title.find_all_next('p'):
+    # Check if previous tag is a header, get it if it is
+    prev = paragh.find_previous()
+    if prev.name == 'h3':
+        #print(prev)
+        output_list.append('<i><h3>' + prev.text + '</h3></i>')
+    #print(paragh)
+    output_list.append('<p>' + paragh.text + '</p><br>')
+
+# Get photo
+aTags = title.find_all_next('a')
+for aTag in aTags:
+    if aTag.has_attr('title') and aTag.has_attr('class'):
+        if aTag['class'][0] == 'thickbox':
+#            print(aTag['title'])
+#            print(aTag['href'])
+            output_list.append('<p class="normal"><img src="' + domain + aTag['href'] + '"><br><i>' + aTag['title'] + '</i></p>' )
+
+# Create output folder <yymmdd>
+folder = project_path + '/' + time.strftime('%Y%m%d')
+if not os.path.exists(folder):
+    os.makedirs(folder)
+    print('Folder ' + folder + ' created.')
+
+'''
+# Save source code of the entire page for debugging
+sourceFile = os.path.join(folder, articleId + '_source.html')
+with open(sourceFile, "w", encoding="utf8") as f:
+    f.write(r.frame.toHtml())
+    print(sourceFile + ' saved.')
+'''
+
+# Using jinja to create html page from extracted data
+templateLoader = FileSystemLoader( searchpath=project_path )
+env = Environment( loader=templateLoader )
+env.globals.update(zip=zip)  #to use zip which can iterate 2 lists
+TEMPLATE_FILE = "tmpl_article.html"
+template = env.get_template( TEMPLATE_FILE )
+output = template.render(heading=heading, output=output_list, oncc_url=oncc_url)
+#print(output)
+#filename = 'oncc_' + time.strftime("%Y%m%d") + '.html'
+filename = os.path.join(folder, articleId)
+with open(filename, "w", encoding="utf-8") as f:
+    f.write(output)
+    print(filename + ' saved.')
---
+++ scrape_index.py
@@ -0,0 +1,113 @@
+import sys
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+from PyQt4.QtWebKit import *
+from lxml import html
+from bs4 import BeautifulSoup
+from jinja2 import Environment, FileSystemLoader
+import time, os
+from datetime import datetime, timedelta
+import shutil
+
+#Take this class for granted.Just use result of rendering.
+class Render(QWebPage):
+  def __init__(self, url):
+    self.app = QApplication(sys.argv)
+    QWebPage.__init__(self)
+    self.loadFinished.connect(self._loadFinished)
+    self.mainFrame().load(QUrl(url))
+    self.app.exec_()
+
+  def _loadFinished(self, result):
+    self.frame = self.mainFrame()
+    self.app.quit()
+
+# Funnction to return weekday in Chinese
+def WeekdayInChinese(Engday):
+    weekday = Engday.upper()
+    if weekday == 'MON':
+        Chiday = '一'
+    elif weekday == 'TUE':
+        Chiday = '二'
+    elif weekday == 'WED':
+        Chiday = '三'
+    elif weekday == 'THU':
+        Chiday = '四'
+    elif weekday == 'FRI':
+        Chiday = '五'
+    elif weekday == 'SAT':
+        Chiday = '六'
+    else:
+        Chiday = '日'
+    return '星期' + Chiday
+
+# Define project home path
+project_path = '.'  # for Windows or RPi interactive
+#project_path = '/home/pi/projects/onccnews'  # for RPi cron job
+
+print('scrape_index starts...')
+domain = 'http://orientaldaily.on.cc'
+# Scape web page by lxml
+r = Render(domain)
+# Parsing data by Beautiful Soup
+soup = BeautifulSoup(r.frame.toHtml(), 'html.parser')
+#print (soup.encode('utf-8'))
+
+# Get cover image
+head_img = soup.find('img', class_='headline')
+head_img_url = domain + head_img['src']
+
+# Get all items from the Drop Down List
+drop_down_list = soup.find('select', id='articleListSELECT')
+# Tag structure of drop_down_list:
+# <select id="articleListSELECT" ...> <optgroup label=...> <option value=...> </option> </optgroup> </select>
+article_type=[]
+article_title=[]
+article_href=[]
+for optgroup in drop_down_list.findAll('optgroup'):
+#    print('inside optgroup')
+#    print(optgroup)
+    # Add group label to the lists
+    article_type.append('G')  # group label
+    article_title.append(optgroup['label'])
+    article_href.append('')
+    for option in optgroup.findAll('option'):
+        # Call scrape_article.py
+        oncc_href = option['value']
+#        cmd = 'python scrape_article.py ' + oncc_href  # for windows
+#        cmd = 'python3 ' + project_path + '/scrape_article.py ' + oncc_href  # for RPi
+#        os.system(cmd)
+        # Add article to the list variables
+        article_type.append('A')  # article
+        article_title.append(option.text)
+        i = option['value'].rfind('/')  # Search last '/' char to get the aricle Id
+        article_href.append(option['value'][i+1:])
+
+# Create output folder <yymmdd>
+folder = project_path + '/' + time.strftime('%Y%m%d')
+if not os.path.exists(folder):
+    os.makedirs(folder)
+    print('Folder ' + folder + ' created.')
+
+# Using jinja to create html page from extracted data
+templateLoader = FileSystemLoader( searchpath=project_path)
+env = Environment( loader=templateLoader )
+env.globals.update(zip=zip)  #to use zip which can iterate 2 lists
+TEMPLATE_FILE = 'tmpl_index.html'
+template = env.get_template( TEMPLATE_FILE )
+newsdate = time.strftime('%d-%m-%Y') + ' ~ ' + WeekdayInChinese(time.strftime('%a'))
+output = template.render(newsdate=newsdate, types=article_type, titles=article_title, hrefs=article_href, head_img_url=head_img_url)
+#print(output)
+filename = os.path.join(folder, 'index.html')
+with open(filename, 'w', encoding='utf-8') as f:
+    f.write(output)
+    print(filename + ' saved.')
+
+# Delete yesterday folder <yymmdd - 1>
+yday = datetime.today() - timedelta(days=1)
+yfolder = project_path + '/' + yday.strftime('%Y%m%d')
+if os.path.exists(yfolder):
+    shutil.rmtree(yfolder, ignore_errors=True)
+    print('Folder ' + yfolder + ' deleted.')
+else:
+   print('Folder ' + yfolder + ' not exists.')
